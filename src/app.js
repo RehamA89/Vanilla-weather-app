d0f8a9887316d9556c10fd4e1da7f605baee5b30
Reham Abdelaal <reham.abdelaal@gmail.com>
2023-07-19 13:27:40 -0700

    Created function to show real-time temperature

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ let currentDate = document.getElementById("date-element");
 
 let date = now.getDate();
 let hours = now.getHours();
-let minutes = now.getMinutes();
+let minutes = now.getMinutes().toString().padStart(2, "0");
 
 let days = [
   "Sunday",
@@ -33,3 +33,13 @@ let months = [
 let month = months[now.getMonth()];
 
 currentDate.innerHTML = `${month} ${day}, ${hours}:${minutes}`;
+
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=Guelph&key=1a87t400bcfaf6fof17c6a4b4b38d75a&units=metric";
+
+function showTemperature(response) {
+  let temperatureValue = document.getElementById(`temperature-value`);
+  temperatureValue.innerHTML = Math.round(response.data.temperature.current);
+}
+
+axios.get(apiUrl).then(showTemperature);
